a3874a8cdbbafe9583e69efd706704ea1c2acaea
Shreya Verma <shreya190991@gmail.com>
2022-02-24 17:00:43 -0600

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css';
+import banner from '../assets/banner.png';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img className="home__image" alt="banner" src={banner} />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAbXFihrI6fB2u74dN1oxl-8Lo8fu8wyXY',
+  authDomain: 'clone-7ed6f.firebaseapp.com',
+  projectId: 'clone-7ed6f',
+  storageBucket: 'clone-7ed6f.appspot.com',
+  messagingSenderId: '816576742587',
+  appId: '1:816576742587:web:5e4cb684cf5d13aee0959e',
+};
